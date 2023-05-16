46a78d681cbb3613f62f1b7b0929d153220c693e
alexblang <67246160+alexblang@users.noreply.github.com>
2023-05-16 15:32:05 +0200

    Export Überblick von Aufgabe 1 in seperate Datei

--- aufgabe_1_overview.py
+++ aufgabe_1_overview.py
@@ -1,5 +1,7 @@
 import pandas as pd # für Datenimport
 import os # für Dateipfade
+import sys
+from datetime import datetime # um Datum und Uhrzeit im Überblick auszugeben
 
 
 
@@ -12,24 +14,10 @@ contacts_file = "data/contacts.csv"
 contacts_path = os.path.join(os.getcwd(), contacts_file)
 
 
-# Datensätze laden, Übersicht anzeigen und Datentypen ausgeben
+# Datensätze als Pandas-Dataframes laden 
 customers_data = pd.read_csv(customers_path)
-print("\nÜbersicht von Kunden-Datensatz:", )
-print(customers_data.head())
-print("\nDatentypen im Kunden-Datensatz:")
-print(customers_data.dtypes)
-
 offers_data = pd.read_csv(offers_path)
-print("\nÜbersicht von Angebote-Datensatz:")
-print(offers_data.head())
-print("\nDatentypen im Angebote-Datensatz:")
-print(offers_data.dtypes)
-
 contacts_data = pd.read_csv(contacts_path)
-print("\nÜbersicht von Kontakte-Datensatz:")
-print(contacts_data.head())
-print("\nDatentypen im Kontakte-Datensatz:")
-print(contacts_data.dtypes)
 
 
 # Datensätze aufräumen
@@ -43,6 +31,49 @@ for index, row in customers_data.iterrows():
 # Entscheidung dafür, die ersten beiden Spalten zu streichen
 
 customers_data = customers_data.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1) # drop-Funktion gibt es ein neues DataFrame aus
+
+
+# Hier wird der Überblick gespeichert
+output_file = "data_overview.txt"
+
+# Öffnen der Ausgabedatei im Schreibmodus
+with open(output_file, "w") as file:
+    # Umleitung der Standardausgabe auf die Datei
+    original_stdout = sys.stdout
+    sys.stdout = file
+
+    print("Datum und Uhrzeit:", datetime.now())
+    
+    print("DATENSATZ CUSTOMERS")
+    print("Übersicht von Kunden-Datensatz\n")
+    print(customers_data.info())
+    print("\nDatentypen im Kunden-Datensatz\n")
+    print(customers_data.dtypes)
+
+    print("\n\nDATENSATZ OFFERS")
+    print("Übersicht von Angebote-Datensatz\n")
+    print(offers_data.info())
+    print("\nDatentypen im Angebote-Datensatz\n")
+    print(offers_data.dtypes)
+
+    print("\n\nDATENSATZ CONTACTS")
+    print("\n\nÜbersicht von Kontakte-Datensatz\n")
+    print(contacts_data.info())
+    print("\nDatentypen im Kontakte-Datensatz\n")
+    print(contacts_data.dtypes)
+
+    # Rückkehr zur Standardausgabe
+    sys.stdout = original_stdout
+
+# Bestätigungsnachricht
+print(f"Der Überblick über die Daten wurde in die Datei {output_file} geschrieben.")
+
+
+# Übersicht anzeigen und Datentypen ausgeben
+
+
+
+
 
 
 """ # Statistische Zusammenfassung der numerischen Spalten im Kunden-Datensatz anzeigen
